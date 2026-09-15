2c523225e1df5b0fa70e2a1e308e7afd28462356
aaprilliawlndri <aprilliya500@gmail.com>
2026-09-15 13:57:48 +0700

    o

--- database/migrations/2026_09_14_065522_add_notification_type_to_applications_table.php
+++ database/migrations/2026_09_14_065522_add_notification_type_to_applications_table.php
@@ -6,15 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('applications', function (Blueprint $table) {
-            $table->string('notification_type')
-                ->nullable()
-                ->after('is_active');
+            $table->string('notification_type')->nullable()->after('is_active');
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('applications', function (Blueprint $table) {
